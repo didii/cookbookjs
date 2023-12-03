9bdecc0ab18da874546b6afe478b2c00a2e1f143
didii <dieter.vbroeck@gmail.com>
2023-12-03 17:58:27 +0100

    Add recipe-update endpoint

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,4 +1,5 @@
 export * from './recipe-create';
 export * from './recipe-get';
+export * from './recipe-update';
 export * from './unit-create';
 export * from './unit-get';
---
+++ server/src/api/recipe-update.ts
@@ -0,0 +1,52 @@
+import { RecipeCreateDto } from 'cookbook-shared/dtos';
+import { ObjectId } from 'mongodb';
+import { Collection } from '~const';
+import { createEndpoint } from '~helpers/create-endpoint';
+
+export const recipeUpdate = createEndpoint(function recipeUpdate(app, db) {
+  app.put('/api/recipe/:id', async (req, res) => {
+    const id = req.params.id;
+    const recipesCollection = db.collection(Collection.Recipes);
+
+    if (!ObjectId.isValid(id)) {
+      return res.sendError({
+        type: 'generic',
+        errorType: 'InvalidId',
+        message: 'Invalid ID',
+        status: 400,
+        data: {
+          id,
+        },
+      });
+    }
+
+    try {
+      const _id = new ObjectId(id);
+      const recipe = await recipesCollection.findOne({ _id });
+      if (recipe == null) {
+        return res.status(404).send('Not found');
+      }
+
+      const validation = RecipeCreateDto.parse(req.body);
+      if (!validation.success) {
+        return res.status(400).send(validation.error);
+      }
+
+      const dto = validation.data;
+      const newRecipe: RecipeCreateDto = {
+        name: dto.name,
+        description: dto.description,
+        steps: dto.steps?.map((s) => ({ text: s.text! })) ?? [],
+        ingredients: dto.ingredients?.map((i) => ({ text: i.text! })) ?? [],
+      };
+
+      const result = await recipesCollection.replaceOne({ _id }, newRecipe);
+      if (result.acknowledged) {
+        return res.status(204).send();
+      }
+      res.sendError({ type: 'mongoDb' });
+    } catch (error) {
+      res.sendError({ type: 'unknown', error });
+    }
+  });
+});
